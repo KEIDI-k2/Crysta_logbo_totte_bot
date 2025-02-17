5f9cee3398877419fa2947c60fd55204d20bfafb
KEIDI-ケイヂ <mitukai2139@gmail.com>
2025-02-17 15:16:00 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import os
 import datetime
 
 # GitHub Secretsから環境変数を取得
-ACCESS_TOKEN = os.getenv("U69q6ggIQHsW15ggKcfp5TkSL3GJzHadCuDEmSbde4E")
-INSTANCE_URL = os.getenv("https://kmy.blue/@keidi")
+ACCESS_TOKEN = os.getenv("MASTODON_ACCESS_TOKEN")
+INSTANCE_URL = os.getenv("MASTODON_INSTANCE_URL")
 
 # Mastodon API クライアントのセットアップ
 mastodon = Mastodon(access_token=ACCESS_TOKEN, api_base_url=INSTANCE_URL)
